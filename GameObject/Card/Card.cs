9d7c7e849eea14d57101c6102c843e97a64ae531
csf1598 <csf1598@rit.edu>
2025-02-19 13:53:00 -0500

    started to create the code of cards. need to decide interface of the cardeffects

---
+++ GameObject/Card/Card.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlyDeck.GameObject.Card
+{
+    internal enum CardType
+    {
+        Header,
+        Footer,
+        Picture,
+        Graph,
+        Transition
+    }
+
+    internal class Card : GameObject
+    {
+        private string name;
+        private string description;
+        private int stat1;
+        private int stat2;
+        private int cost;
+
+        public Card(Vector2 position, Texture2D texture) : base(position, texture)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameObject/GameObject.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlyDeck.GameObject
+{
+    /// <summary>
+    /// Base class for any player-facing objects to inherit from (i.e visible to the player)
+    /// </summary>
+    internal abstract class GameObject
+    {
+        private Vector2 position;
+        private Texture2D texture;
+
+        public Vector2 Position { get { return position; } }
+        public Texture2D Texture { get { return texture; } }
+
+        /// <summary>
+        /// Creates a new GameObject
+        /// </summary>
+        /// <param name="position">The position of the game object</param>
+        /// <param name="texture">The texture of the game object</param>
+        public GameObject(Vector2 position, Texture2D texture)
+        {
+            this.position = position;
+            this.texture = texture;
+        }
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
