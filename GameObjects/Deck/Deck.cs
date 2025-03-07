952b033a3e6385e2de43d81cfef61f1ea3b03064
Ben Haines <bwh8037@rit.edu>
2025-03-07 13:55:35 -0500

    Created the Deck class. The deck class involves a list of Card objects. The list can be shuffled, a card can be added to the top or bottom, and a card can be removed at any index (default 0). The top card can also be peeked.

---
+++ GameObjects/Deck/Deck.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlyDeck.GameObjects.Deck
+{
+    internal class Deck
+    {
+        // -- Fields -- \\
+
+        private List<Card.Card> cards;
+        private Random shuffle;
+
+        // -- Properties -- \\
+
+        /// <summary>
+        /// Gets the size of the deck.
+        /// </summary>
+        public int DeckSize { get { return cards.Count; } }
+        /// <summary>
+        /// Gets the top card of the deck
+        /// </summary>
+        public Card.Card TopCard { get { return cards[0]; } }
+
+
+        // -- Constructor -- \\
+
+        /// <summary>
+        /// Create a new deck of cards.
+        /// </summary>
+        /// <param name="cards">The cards that the deck will use.</param>
+        public Deck(List<Card.Card> cards)
+        {
+            this.cards = cards;
+            shuffle = new Random();
+            Shuffle();
+        }
+
+        // -- Methods -- \\
+
+        /// <summary>
+        /// Draw a card from the deck of a specified cardNum, 0 being the top of the deck.
+        /// </summary>
+        /// <param name="cardNum">The number of the card to draw.</param>
+        /// <returns>The Card object drawn.</returns>
+        public Card.Card DrawCard(int cardNum)
+        {
+            Card.Card cardDrawn = cards[cardNum];
+            cards.RemoveAt(cardNum);
+            return cardDrawn;
+        }
+
+        /// <summary>
+        /// Draw the card on the top of the deck, index 0.
+        /// </summary>
+        /// <returns>The Card object drawn.</returns>
+        public Card.Card DrawCard()
+        {
+            return DrawCard(0);
+        }
+
+        /// <summary>
+        /// Adds a specified card to the bottom of the deck.
+        /// </summary>
+        /// <param name="toAdd">The Card object to add.</param>
+        public void AddCardBottom(Card.Card toAdd)
+        {
+            cards.Add(toAdd);
+        }
+
+        /// <summary>
+        /// Adds a specified card to the top of the deck.
+        /// </summary>
+        /// <param name="toAdd">The Card object to add.</param>
+        public void AddCardTop(Card.Card toAdd)
+        {
+            cards.Insert(0, toAdd);
+        }
+
+        /// <summary>
+        /// Shuffle the deck of cards by randomly ordering them in a new deck.
+        /// </summary>
+        public void Shuffle()
+        {
+            List<Card.Card> newCards = new();
+            while (cards.Count != 0)
+            {
+                int randomIndex = shuffle.Next(0, cards.Count);
+                newCards.Add(cards[randomIndex]);
+                cards.RemoveAt(randomIndex);
+            }
+            cards.AddRange(newCards);
+        }
+    }
+}
