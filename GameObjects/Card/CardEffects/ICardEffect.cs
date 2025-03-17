3d0fdd20d3dc80155b85f735af9f1d64f571a5f1
csf1598 <csf1598@MSS2150-26>
2025-03-17 15:52:01 -0400

    Started working on the card. Going to need the content manager before I move further forward on the UI parts of this.

---
+++ GameObjects/Card/CardEffects/ICardEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Author: Cooper Fleishman
+namespace SlyDeck.GameObjects.Card.CardEffects
+{
+    /// <summary>
+    /// An interface for all different card effects
+    /// </summary>
+    internal interface ICardEffect
+    {
+        /// <summary>
+        /// Performs the logic this effect contains
+        /// </summary>
+        public void Perform();
+    }
+}
