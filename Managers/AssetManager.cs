82e401f41d17f914393fd9d4f2aec3b975fc1ad0
Vinny Keeler <101831569+ShieldedAsh@users.noreply.github.com>
2025-03-17 14:14:27 -0400

    Init AssetManager class

---
+++ Managers/AssetManager.cs
@@ -0,0 +1,42 @@
+﻿//System Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//MonoGame Imports
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SlyDeck.Managers
+{
+    //Authors: Vinny Keeler
+    class AssetManager
+    {
+        //Singleton
+        private static AssetManager instance;
+        public static AssetManager Instance { get { return instance; } }
+
+        private static AssetManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AssetManager();
+            }
+
+            return instance;
+        }
+
+        //dictionaries
+
+        private Dictionary<string, Texture2D> textures;
+        private Dictionary<string, SpriteFont> fonts;
+
+        //methods
+
+        public void GetAsset<T>(string assetName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
