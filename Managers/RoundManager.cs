9266f61b0b22b121e6dfd6ad8e3a65ab0f29aa7c
Vinny Keeler <101831569+ShieldedAsh@users.noreply.github.com>
2025-03-26 13:59:12 -0400

    Init RoundManager, made it a singleton

---
+++ Managers/RoundManager.cs
@@ -0,0 +1,43 @@
+﻿//system imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//monogame imports
+using SlyDeck.GameObjects;
+using SlyDeck.GameObjects.Card;
+using SlyDeck.GameObjects.Card.CardEffects;
+using SlyDeck.GameObjects.UI;
+
+namespace SlyDeck.Managers
+{
+    enum Turn
+    {
+        Player,
+        Enemy,
+        None
+    }
+    
+    //Author: Vinny Keeler
+    internal class RoundManager
+    {
+        //Singleton
+        private static RoundManager instance;
+        public static RoundManager Instance
+        {
+            get { return GetInstance(); }
+        }
+
+        private static RoundManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RoundManager();
+            }
+
+            return instance;
+        }
+    }
+}
